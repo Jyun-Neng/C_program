56fd9aff93a9492e46dae25b1164526c9da20ac2
Jyun-NengJi <jyunnengji@gmail.com>
2017-05-02 00:19:19 +0800

    Create two dimensions dynamic array

---
+++ two_dim_dynamic_array.cpp
@@ -0,0 +1,41 @@
+/* Create a two dimensions dynamic array.
+
+	date 2017/05/02
+	version 1
+*/
+#include <iostream>
+
+using namespace std;
+
+void TwoDimArray(int **A, int *m, int *n)
+{
+	A = new int *[*m];	// Create an array of address.
+	for (int i = 0; i < *m; ++i){
+		A[i] = new int [*n];	// Create an array of data.
+	}
+
+	for (int i = 0; i < *m; ++i){
+		for (int j = 0; j < *n; ++j){
+			A[i][j] = i + j + 1;
+		}	
+	}
+	for (int i = 0; i < *m; ++i){
+		for (int j = 0; j < *n; ++j){
+			printf("%3d ", A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int row, column;
+	int *A;
+
+	row = 10;
+	column = 10;
+
+	TwoDimArray(&A, &row, &column);
+
+	return 0;
+}
