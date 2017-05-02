3f0e78bbaaf9607c0510c2ae5a615489b6ae334e
Jyun-NengJi <jyunnengji@gmail.com>
2017-05-02 17:37:21 +0800

    use vector.

--- two_dim_dynamic_array.cpp
+++ two_dim_dynamic_array.cpp
@@ -1,11 +1,31 @@
 /* Create a two dimensional dynamic array.
+   Using vector to create a two dimensional dynamic array.
 
 	date 2017/05/02
-	version 1
+	version 2
+
 */
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void TwoDimArray_vector(int &m, int &n)
+{
+	vector<int> row;
+	row.assign(m, 0);	// Create a 1-D dynamic array.	
+	vector< vector<int> > array_2D;
+	array_2D.assign(n, row);	// Create a 2-D dynamic array.
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			printf("%d ", array_2D.at(j).at(i));	// columnXrow not rowXcolumn. i.e .at(column).at(row)
+		}
+		printf("\n");
+	}
+
+}
 
 void TwoDimArray(int **A, int *m, int *n)
 {
@@ -33,9 +53,10 @@ int main(int argc, char const *argv[])
 	int *A;
 
 	row = 10;
-	column = 10;
+	column = 5;
 
 	TwoDimArray(&A, &row, &column);
+	TwoDimArray_vector(row, column);
 
 	return 0;
 }
