03cf6aa83a301bcce5634f9adf44dfcbd761b542
Jyun-NengJi <jyunnengji@gmail.com>
2017-05-04 23:02:58 +0800

    Can find the inverse of the congruence.

--- find_gcd.cpp
+++ find_gcd.cpp
@@ -1,19 +1,20 @@
 /* Using Euclidean algorithm to find the greatest common divisor. 
+   Can also find the inverse of the congruence, using extended Euclidean algorithm.
 
-   Date: 2017/05/01
-   version 1 
+   Date: 2017/05/04
+   version 2
 */
 #include <iostream>
 
 using namespace std;
 
-int gcd(int *a, int *b)
+int gcd(int a, int b)
 {
 	int big_num, small_num;
 	int q, r;
 
-	big_num = (*a > *b) ? *a : *b;
-	small_num = (big_num == *a) ? *b : *a;
+	big_num = (a > b) ? a : b;
+	small_num = (big_num == a) ? b : a;
 
 	// Euclidean algorithm
 	while(small_num > 0){
@@ -25,11 +26,51 @@ int gcd(int *a, int *b)
 	return big_num;
 }
 
+int ExtendedEuclideanAlgo(int a, int b)	// find the inverse of b mod a.
+{
+	int q, r, x, x1, x2, y, y1, y2, ori_a;
+
+	x2 = 1;
+	x1 = 0;
+	y2 = 0;
+	y1 = 1;
+	ori_a = a;
+
+	while(b > 0)
+	{
+		q = a / b;
+		r = a % b;
+		x = x2 - q * x1;
+		y = y2 - q * y1;
+		x2 = x1;
+		x1 = x;
+		y2 = y1;
+		y1 = y;
+		a = b;
+		b = r;
+	}
+	y = y2;
+	y = (y < 0) ? ori_a + y : y;	// let the inverse is between 0 to a. 
+	return y;
+}
+
 int main(int argc, char const *argv[])
 {
-	int a = 103, b = 209;
-	int gcd_num;
-	gcd_num = gcd(&a, &b);
+	int a = 1759, b = 550;
+	int gcd_num, inverse;
+	gcd_num = gcd(a, b);
 	printf("%d\n", gcd_num);
+
+	if (gcd_num == 1)	// find the inverse.
+	{
+		inverse = ExtendedEuclideanAlgo(a, b);
+		
+		printf("The inverse of %d mod %d is %d\n", b, a, inverse);
+	}
+	else
+	{
+		printf("No inverse.\n");
+	}
+
 	return 0;
 }
